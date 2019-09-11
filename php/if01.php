e8570ba25321fde17e1c4240d344af7feadabd81
jiwonyuk <ykc4791@naver.com>
2019-09-11 16:23:30 +0900

    if-for

---
+++ php/if01.php
@@ -0,0 +1,11 @@
+<?php
+
+$money = 700;
+if ($money>1000) {            //if(조건) 실행
+    echo "콜라";
+    echo "음료수 사먹음.";
+} else if ($money < 800) {
+    echo "빵 사먹는다.";   
+} else{
+    echo "못먹는다.";
+}
---
+++ php/if03.php
@@ -0,0 +1,7 @@
+<?php
+// if(조건) 실행 
+// 3형 연산자
+
+$lang = "ko";
+$title = ($lang == "ko")? "한국어" : "korean";
+echo $title;
